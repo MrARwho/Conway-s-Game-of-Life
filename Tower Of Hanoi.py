a77912e70c90602ae87dcfa48d3dc37fb34bdc1f
MrARwho <22sp-052-cs@students.uituniversity.edu.pk>
2026-05-17 04:32:07 +0500

    updated comments

--- Tower Of Hanoi.py
+++ Tower Of Hanoi.py
@@ -2,8 +2,10 @@ def TOH (numbers , start , mid, end ):
     if numbers == 1:
         print (f"Move disk 1 from {start} to {end}")
         return
+    #move top n-1 disks from start to mid using end
     TOH (numbers - 1 , start , end , mid)
     print (f"Move disk {numbers} from {start} to {end}")
+    #move n-1 disks from mid to end using start
     TOH (numbers - 1 , mid , start , end)
     
 disks = 5
